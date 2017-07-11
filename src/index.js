2230b9247e04597af2d59a23315112d5941c4329
Pablo Jurado <pjurado@overnitecbt.com>
2017-07-11 13:30:33 -0500

    clean up code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+
+class App extends React.Component {
+  componentDidMount () {
+    let canvas = ReactDOM.findDOMNode(this.refs.myCanvas)
+    let ctx = canvas.getContext('2d')
+
+    ctx.fillStyle = 'rgb(200,0,0)'
+    ctx.fillRect(0, 0, 20, 20)
+  }
+
+  render () {
+    return (
+      <div>
+        <canvas ref='myCanvas' width='500' height='600' />
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
